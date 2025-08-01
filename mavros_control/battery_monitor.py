766271991f3356515062f6bebca5808bbed3d7ca
chanevan10 <chanevan10@gmail.com>
2025-08-01 10:47:31 -0500

    beta 0.1 for battery_monitor

--- mavros_control/battery_monitor.py
+++ mavros_control/battery_monitor.py
@@ -6,9 +6,33 @@ from sensor_msgs.msg import BatteryState
 class BatteryMonitorNode(Node):
     def __init__(self):
         super.__init__()
+        self.volts = 0.0
+        self.volt_buffer = []
+        self.current = 0.0
 
-        # TODO create subscriber to battery state node and create a timer that gets voltage every 30s
+        self.volt_subscriber = self.create_subscription(BatteryState, '/mavros/battery', self.battery_callback, 10)
+        self.timer = self.create_timer(30,self.reading_callback)
+    
+    def battery_callback(self, msg: BatteryState):
+        
+        # TODO: get necessary information from battery topic
+        self.volts = msg.voltage
+        self.current = msg.current
+        pass
+
+    def reading_callback(self):
+
+        # TODO: check voltage against threshold, add it to the buffer
+        self.volt_buffer.append(self.volts)
+        if len(self.volt_buffer) > 5:
+            self.volt_buffer.pop(0)
+        if sum(self.volt_buffer)/5.0 < 12.0:
+            self.get_logger().warn("VOLTAGE READING BELOW 12, CHARGE SOON")
+        pass
 
 def main():
     node = BatteryMonitorNode()
     rclpy.spin_once(node)
+
+if __name__ == '__main__':
+    main()
